0fcf922bd7f8049df5777e1934c220208ec23050
dimaperss <93925414+dimaperss@users.noreply.github.com>
2021-11-09 14:25:50 +0300

    Add files via upload

---
+++ Lab1_3.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <iomanip>
+#include <list>
+#include <vector>
+#include <string>
+#include <clocale>
+using namespace std;
+
+class Stock{
+    string company_name;
+    int stock_num;
+    vector<float> stock_cost_array;
+    float stock_cost;
+    double all_stock_cost;
+
+    void SetTotal(){
+    all_stock_cost = 0;
+    for (int i = 0; i < stock_num; i++){
+        all_stock_cost += stock_cost_array[i];
+        }
+    }
+
+    public:
+
+    Stock(){
+        cout << "Конструктор без параметров создан \n" << endl;
+        company_name = "First company";
+        stock_num = 1;
+        all_stock_cost = 10;
+    }
+
+    Stock(string s, int i){
+        cout << "Конструктор с параметрами создан" << endl;
+        company_name = s;
+        stock_num = i;
+        cout << "Введите стоимость каждой акции:" << endl;
+        for (int i = 0; i < stock_num; i++) {
+            int s_cost;
+            cout << "Акция номер " << i+1 << " : ";
+            cin >> s_cost;
+            stock_cost_array.push_back(s_cost);
+        }
+        SetTotal();
+    }
+
+    int Buy(int s_num){
+        if (s_num > 0){
+            float s_cost;
+            for (int i = 0; i < s_num; i++){
+                cout << "Цена приобретённой акции №" << i + 1 << ": ";
+                cin >> s_cost;
+                stock_cost_array.push_back(s_cost);
+                stock_num++;
+            }
+        } else{
+            return 1;
+        }
+        return 0;
+    }
+
+    int Sell(int s_num){
+        if (s_num > 0){
+            int ndx;
+            for (int i = 0; i < s_num; i++){
+                cout << "Номер проданной акции: ";
+                cin >> ndx;
+                ndx++;
+                for (int i = ndx; i < stock_num - 1; i++){
+                    stock_cost_array[i] = stock_cost_array[i + 1];
+                }
+                stock_cost_array.pop_back();
+                stock_num--;
+            }
+        } else {
+            return 1;
+        }
+        return 0;
+    }
+
+    void Update(int ndx, float cost){
+        stock_cost_array[ndx] = cost;
+        SetTotal();
+    }
+
+    void Show(){
+        cout << "Название компании: " << setw(1) << company_name << endl;
+        cout << "Общее количество акций: " << setw(1) << stock_num << endl;
+        cout << "Общая стоимость всех акций: " << setw(1) << all_stock_cost << endl;
+    }
+};
+
+
+
+void stock_test() {
+    vector<Stock> stocks_array;
+    Stock a("a", 2), b("b", 2), c("c", 2), d("d", 2), e("e", 2);
+    stocks_array.push_back(a);
+    stocks_array.push_back(b);
+    stocks_array.push_back(c);
+    stocks_array.push_back(d);
+    stocks_array.push_back(e);
+    a.Buy(2);
+    for (int i = 0; i < stocks_array.size(); i++){
+        cout << endl;
+        stocks_array[i].Show();
+    }
+}
+
+int main(){
+    setlocale(LC_ALL,"Russian");
+    stock_test();
+
+    return 0;
+}
---
+++ Lab1_4.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+#include <clocale>
+using namespace std;
+
+class Train {
+	string PN;
+	int TrainNumber;
+	int StartNumber;
+	int	CountOM;
+	int	Kupe;
+	int Platskart;
+
+    public:
+        Train() {
+            PN = "Unkown";
+            TrainNumber = 0;
+            StartNumber = 0;
+            CountOM = 0;
+            Kupe = 0;
+            Platskart = 0;
+        }
+        Train( string	a, int c, int d, int e, int f, int g) {
+            PN = a;
+            TrainNumber = c;
+            StartNumber = d;
+            CountOM = e;
+            Kupe = f;
+            Platskart = g;
+        }
+        ~Train() {}
+
+        void SetDriverName(string s) {
+            PN = s;
+        }
+        void SetPlatskart(int a) {
+            Platskart = a;
+        }
+        void SetTrainNumber(int a) {
+            TrainNumber = a;
+        }
+        void SetStartNumber(int a) {
+            StartNumber = a;
+        }
+        void SetCountOM(int a) {
+            CountOM = a;
+
+        }
+        void SetKupe(int a) {
+            Kupe = a;
+        }
+
+        string GetPN(){
+            return PN;
+        }
+        int GetPlatskart() {
+            return Platskart;
+        }
+        int GetTrainNumber() {
+            return TrainNumber;
+        }
+        int GetStartNumber() {
+            return StartNumber;
+        }
+        int GetCountOM() {
+            return CountOM;
+        }
+        int GetKupe() {
+            return Kupe;
+        }
+
+        void Show(){
+            cout << "Пункт назначения: " << setw(1) << PN << endl;
+            cout << "Номер поезда: " << setw(1) << TrainNumber << endl;
+            cout << "Время отправления: " << setw(1) << StartNumber << endl;
+            cout << "Число общих мест: " << setw(1) << CountOM << endl;
+            cout << "Купейных: " << setw(1) << Kupe << endl;
+            cout << "Плацкартных: " << setw(1) << Platskart << endl;
+        }
+};
+
+int main() {
+    setlocale(LC_ALL,"Russian");
+	vector<Train> TrainArr;
+
+	Train a("Москва", 585, 10*60+10, 20, 585, 15);
+	Train	b("Питер", 228, 13*60+5, 22, 228, 122);
+	Train	c("Ростов", 982, 8*60+0, 0, 982, 18);
+	Train	d("Ростов-на-Дону", 100, 1*60+55, 1, 100, 0);
+
+	TrainArr.push_back(a);
+	TrainArr.push_back(b);
+	TrainArr.push_back(c);
+	TrainArr.push_back(d);
+	for (int i = 0; i < TrainArr.size(); i++){
+        if (TrainArr[i].GetPN() == "Ростов-на-Дону") {
+			TrainArr[i].Show();
+			cout << endl;
+		}
+		if ((TrainArr[i].GetPN() == "Питер")&&(TrainArr[i].GetStartNumber()>7*60)) {
+			TrainArr[i].Show();
+			cout << endl;
+		}
+		if ((TrainArr[i].GetPN() == "Москва")&&(TrainArr[i].GetCountOM()>0)) {
+			TrainArr[i].Show();
+			cout << endl;
+		}
+	}
+
+	return 0;
+}
